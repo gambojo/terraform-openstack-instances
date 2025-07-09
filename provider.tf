35bb3304f751c89113816ddc38589e06be9b4a28
Agamov Gamid <agamovga@cloudx.group>
2025-07-09 21:28:42 +0300

    Add s3 minio backend

--- provider.tf
+++ provider.tf
@@ -1,18 +1,18 @@
 terraform {
-  backend "s3" {
-    endpoint = "http://minio-api01.apps.k8s-1.cp.dev.cldx.ru"
-    bucket = "terraform"
-    region = "us-east-1"
-    key    = "gama/terraform.tfstate"
+  # backend "s3" {
+  #   endpoint = "http://minio-api01.apps.k8s-1.cp.dev.cldx.ru"
+  #   bucket = "terraform"
+  #   region = "us-east-1"
+  #   key    = "gama/terraform.tfstate"
 
-    access_key = "65vUNEJv3hW3p499uueU"
-    secret_key = "jzHDtyOe6ZJspxwVypf2THchm7BEp1jJ1qskz1KB"
+  #   access_key = "65vUNEJv3hW3p499uueU"
+  #   secret_key = "jzHDtyOe6ZJspxwVypf2THchm7BEp1jJ1qskz1KB"
 
-    skip_region_validation      = true
-    skip_credentials_validation = true
-    force_path_style            = true 
-    skip_metadata_api_check     = true
-  }
+  #   skip_region_validation      = true
+  #   skip_credentials_validation = true
+  #   force_path_style            = true 
+  #   skip_metadata_api_check     = true
+  # }
 
   required_providers {
     openstack = {
